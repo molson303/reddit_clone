ddbd56a0f967b5244db9e61264bfa537cbf4e468
MIchael <michael.olson756@gmail.com>
2016-08-11 16:40:32 -0600

    arrows up vote and down vote

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var app = angular.module('firstApp', []);
 app.controller("ExercisesController", function($scope){
-      console.log();
-  })
+  $scope.counter = {}
+  $scope.counter.tracker = 0;
+
+  $scope.counterPos = function(){
+
+      $scope.counter.tracker = $scope.counter.tracker += 1;
+
+    }
+  $scope.counterNeg = function(){
+      $scope.counter.tracker = $scope.counter.tracker -= 1;
+      
+  }
+
+})
