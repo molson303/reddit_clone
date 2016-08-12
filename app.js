baa87b0583711b617860e50441f0db6bdd41afcc
MIchael <michael.olson756@gmail.com>
2016-08-11 20:29:00 -0600

    modal complete

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var app = angular.module('firstApp', []);
+var app = angular.module('firstApp', ['ngAnimate']);
 app.controller("ExercisesController", function($scope){
   $scope.counter = {}
   $scope.counter.tracker = 0;
 
-
   $scope.counterPos = function(post){
-    post.votes = post.votes + 1;
+    post.votes ++;
     }
   $scope.counterNeg = function(post){
-    post.votes = post.votes - 1;
+    post.votes --;
     }
-
 })
